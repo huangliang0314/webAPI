74a6ba5a022028c43dd2740ab70e7cf8ebfc147f
huangliang0314 <909589592@qq.com>
2018-03-13 18:43:30 +0800

    first commit

---
+++ WebAPI/Models/BookChapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class BookChapter
+    {
+        public int Number { get; set; }
+
+        public string Title { get; set; }
+
+        public int Pages { get; set; }
+    }
+}
