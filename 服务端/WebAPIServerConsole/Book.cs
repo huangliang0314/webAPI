3c913858b8ae538e605245031df21686e340a999
huangliang0314 <909589592@qq.com>
2018-03-15 19:24:29 +0800

    web api 自驻留

---
+++ 服务端/WebAPIServerConsole/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Xml.XmlConfiguration;
+using System.Xml.Serialization;
+
+namespace WebAPIServerConsole
+{
+    
+    [Serializable]
+    [XmlSerializerAssembly("hl")]
+    [XmlRoot]
+    public class Book
+    {
+        [XmlElement]
+        /// <summary>
+        /// 序号
+        /// </summary>
+        public int id { get; private set; }
+
+        [XmlElement]
+        /// <summary>
+        /// 书名
+        /// </summary>
+        public string title { get; private set; }
+
+       
+
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string title)
+        {
+            this.id = id;
+            this.title = title;
+        }
+
+    }
+}
---
+++ 服务端/WebAPIServerConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using System.Web.Http.SelfHost.Channels;
+
+namespace WebAPIServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var config = new HttpSelfHostConfiguration("http://localhost:8081");
+            config.MapHttpAttributeRoutes();
+            using (var server =new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("服务已启动...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    [RoutePrefix("Books")]
+    public class BookController : ApiController
+    {
+        [Route("TheOne")]
+        public IEnumerable<Book> GetBooks()
+        {
+
+            return new List<Book>()
+            {
+                    new Book(1,"gaojibianc"),
+                    new Book(id:2,title:"噶系边吃")
+            };
+        }
+
+
+    }
+}
